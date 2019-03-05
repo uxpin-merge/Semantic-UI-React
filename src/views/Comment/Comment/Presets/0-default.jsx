c66c66606ea1e19f153a30c1dc1cde00b9e2eaa6
rtaraszka <radoslaw.taraszka@gmail.com>
2019-03-05 22:22:27 +0100

    udpated comment styles

--- src/views/Comment/Comment/Presets/0-default.jsx
+++ src/views/Comment/Comment/Presets/0-default.jsx
@@ -6,23 +6,26 @@ import CommentContent from '../../CommentContent'
 import CommentAuthor from '../../CommentAuthor'
 // import CommentMetadata from '../CommentMetadata'
 import CommentText from '../../CommentText'
+import CommentGroup from '../../CommentGroup/CommentGroup'
 // import CommentGroup from '../../CommentGroup/CommentGroup'
 // import Icon from '../../../elements/Icon/Icon'
 
 export default (
-  <Comment uxpId='00'>
-    <CommentAvatar
-      as='a'
-      src='https://react.semantic-ui.com/images/avatar/small/stevie.jpg'
-      uxpId='01'
-    />
-    <CommentContent uxpId='02'>
-      <CommentAuthor as='a' uxpId='03'>
-        Stevie Feliciano
-      </CommentAuthor>
-      <CommentText uxpId='04'>
-        Hey guys, I hope this example comment is helping you read this documentation.
-      </CommentText>
-    </CommentContent>
-  </Comment>
+  <CommentGroup uxpId='0'>
+    <Comment uxpId='00'>
+      <CommentAvatar
+        as='a'
+        src='https://react.semantic-ui.com/images/avatar/small/stevie.jpg'
+        uxpId='01'
+      />
+      <CommentContent uxpId='02'>
+        <CommentAuthor as='a' uxpId='03'>
+          Stevie Feliciano
+        </CommentAuthor>
+        <CommentText uxpId='04'>
+          Hey guys, I hope this example comment is helping you read this documentation.
+        </CommentText>
+      </CommentContent>
+    </Comment>
+  </CommentGroup>
 )
