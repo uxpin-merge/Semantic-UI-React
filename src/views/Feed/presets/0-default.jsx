95a1a2d92bc4e3ee338c66f37fafb3007c3e8989
Radek Taraszka <radoslaw.taraszka@uxpin.com>
2019-03-21 13:09:47 +0100

    modified circleCi config

--- src/views/Feed/presets/0-default.jsx
+++ src/views/Feed/presets/0-default.jsx
@@ -2,26 +2,6 @@
 import * as React from 'react'
 import Feed from '../Feed'
 
-// export default (
-//   <FeedEvent uxpId='FeedEvent0'>
-//     <FeedLabel image='https://react.semantic-ui.com/images/avatar/small/helen.jpg' uxpId='FeedLabel0' />
-//     <FeedContent uxpId='FeedContent0'>
-//       <FeedSummary uxpId='FeedSummary0'>
-//         <FeedUser uxpId='FeedUser0'>Joe Henderson</FeedUser>
-//         <FeedDate uxpId='FeedDate0'>3 days ago</FeedDate>
-//       </FeedSummary>
-//       <FeedExtra text uxpId='FeedExtra0'>
-//         Ours is a life of constant reruns. We're always circling back to where we'd we started,
-//         then starting all over again. Even if we don't run extra laps that day, we surely will
-//         come back for more of the same another day soon.
-//       </FeedExtra>
-//       <FeedMeta uxpId='FeedMeta0'>
-//         <FeedLike icon='like' uxpId='FeedLike0'>5 Likes</FeedLike>
-//       </FeedMeta>
-//     </FeedContent>
-//   </FeedEvent>
-// )
-
 const events = [
   {
     date: '1 Hour Ago',
