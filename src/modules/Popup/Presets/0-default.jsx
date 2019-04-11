a9f4f2b6764d1e264a7eafe5ebe3f3d5cd8d2f13
Radek Taraszka <radoslaw.taraszka@uxpin.com>
2019-04-11 10:40:15 +0200

    jsx update

--- src/modules/Popup/Presets/0-default.jsx
+++ src/modules/Popup/Presets/0-default.jsx
@@ -3,4 +3,6 @@ import * as React from 'react'
 import Popup from '../Popup'
 import Button from '../../../elements/Button/index'
 
-export default <Popup trigger={<Button icon='add' uxpId='0' />} content='Add users to your feed' />
+export default (
+  <Popup trigger={<Button icon='add' uxpId='1' />} content='Add users to your feed' uxpId='0' />
+)
